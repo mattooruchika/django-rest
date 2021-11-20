318e6e3bf7c88535bdfb4a85cf53eb3af885082d
Ruchika Mattoo <ruchikamattoo09@gmail.com>
2021-11-20 20:04:03 +0530

    added viewset serializers for coursedb

--- genericCourseSerializers/genericCourseSerializerApp/urls.py
+++ genericCourseSerializers/genericCourseSerializerApp/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import urlpatterns
+from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from genericCourseSerializerApp.views import *
 
+router = DefaultRouter()
+router.register('course', CourseViewSet)
+
+# for viewset
 urlpatterns = [
-    path('course/', view=CourseList.as_view()),
-    path('course/<int:pk>', view=CourseDetail.as_view()),
+    path('', include(router.urls)),
 ]
+
+
+
+# GENERICS
+# urlpatterns = [
+#     path('course/', view=CourseList.as_view()),
+#     path('course/<int:pk>', view=CourseDetail.as_view()),
+# ]
--- genericCourseSerializers/genericCourseSerializerApp/views.py
+++ genericCourseSerializers/genericCourseSerializerApp/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import viewsets
 
 # Create your views here.
 from genericCourseSerializerApp.models import *
 from genericCourseSerializerApp.serializer import *
 
-
-class CourseList(generics.ListCreateAPIView):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+
+# Generics 
+# class CourseList(generics.ListCreateAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
+
+# class CourseDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
